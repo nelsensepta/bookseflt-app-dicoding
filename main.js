f09de11acdda8435e46c66f26bb9e2f4c5704745
Nelsen Septa <nelsenseptahenidar@gmail.com>
2022-10-21 17:03:51 +0700

    button

--- main.js
+++ main.js
@@ -6,3 +6,24 @@ const formSearchingBook = document.getElementById("searchBook");
 function CheckForStorage() {
   return typeof Storage !== "undefined";
 }
+
+function CreateGreenButton(book, eventListener) {
+  const isSelesai = book.isComplete ? "Belum selesai" : "Selesai";
+
+  const greenButton = document.createElement("button");
+  greenButton.classList.add("green");
+  greenButton.innerText = isSelesai + " di Baca";
+  greenButton.addEventListener("click", function (event) {
+    eventListener(event);
+  });
+  return greenButton;
+}
+function CreateRedButton(eventListener) {
+  const redButton = document.createElement("button");
+  redButton.classList.add("red");
+  redButton.innerText = "Hapus buku";
+  redButton.addEventListener("click", function (event) {
+    eventListener(event);
+  });
+  return redButton;
+}
